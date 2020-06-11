d0827f3fe2b7ec41fd4c33ec8d0aef3c17e1f204
robotslacker <184902652@qq.com>
2020-06-11 22:34:11 +0800

    delete util/rflib

--- linkoopdb/util/rflib/SQLCliLibrary/uploadpypi.bat
+++
@@ -1,8 +0,0 @@
-del /s/q dist\*
-del /s/q build\*
-python setup.py sdist
-python setup.py bdist_wheel --universal
-pip uninstall --yes robotframework-sqlclilibrary
-python setup.py install
-python -m robot.libdoc .\SQLCliLibrary doc\SQLCliLibrary.html
-REM  twine upload dist/*
