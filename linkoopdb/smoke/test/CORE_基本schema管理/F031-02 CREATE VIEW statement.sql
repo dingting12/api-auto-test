ba76a01433127379a88ea1b66c1ccae762a985d8
gengchenyu <chenyu.geng@datapps.com>
2020-06-24 15:04:47 +0800

    修改冒烟测试对比文件

--- linkoopdb/smoke/test/CORE_基本schema管理/F031-02 CREATE VIEW statement.sql
+++ linkoopdb/smoke/test/CORE_基本schema管理/F031-02 CREATE VIEW statement.sql
@@ -1,12 +1,12 @@
 set echo on
 -- 清空环境
 drop view v2 IF  EXISTS;
-drop table t_view  IF  EXISTS;
+drop table t_view_create  IF  EXISTS;
 
 
 
 -- 创建表
-CREATE TABLE IF NOT EXISTS t_view (
+CREATE TABLE IF NOT EXISTS t_view_create (
 EMPNO INT,
 ENAME VARCHAR(20),
 JOB VARCHAR(20),
@@ -18,10 +18,10 @@ DEPTNO INT);
 
 
 -- 插入数据
-INSERT INTO t_view  VALUES (7369,'SMITH','CLERK',7902,TO_DATE('1980-12-17','yyyy-mm-dd'),800,NULL,20);
-INSERT INTO t_view  VALUES (7499,'ALLEN','SALESMAN',7698,TO_DATE('1981-2-20','yyyy-mm-dd'),1600,300,30);
-INSERT INTO t_view  VALUES (7521,'WARD','SALESMAN',7698,TO_DATE('1981-2-2','yyyy-mm-dd'),1250,500,30);
+INSERT INTO t_view_create  VALUES (7369,'SMITH','CLERK',7902,TO_DATE('1980-12-17','yyyy-mm-dd'),800,NULL,20);
+INSERT INTO t_view_create  VALUES (7499,'ALLEN','SALESMAN',7698,TO_DATE('1981-2-20','yyyy-mm-dd'),1600,300,30);
+INSERT INTO t_view_create  VALUES (7521,'WARD','SALESMAN',7698,TO_DATE('1981-2-2','yyyy-mm-dd'),1250,500,30);
 
-create view v2 as select * from t_view order by empno;
+create view v2 as select * from t_view_create order by empno;
 
 select * from v2;
