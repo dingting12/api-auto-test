c9844cdb5327e28643b5365544cba8d48810b6a0
robotslacker <184902652@qq.com>
2020-06-24 18:50:24 +0800

    Update ParseRobotOutput.py

--- linkoopdb/regression/common/ParseRobotOutput.py
+++ linkoopdb/regression/common/ParseRobotOutput.py
@@ -6,22 +6,8 @@ import os
 import sys
 
 
-if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print("Missed parameter:  ParseRobotOutput [output.xml]")
-        sys.exit(0)
-
-    # 解析XML文件
-    suite = ExecutionResult(sys.argv[1]).suite
-
-    # 连接mysql数据库
-    mydb = mysql.connector.connect(
-        host="node10",
-        user="report",
-        passwd="123456",
-        database="report"
-    )
-    for test in suite.tests:  # 遍历所有测试
+def load_testresults(p_dbHandler, p_szSuite):
+    for test in p_szSuite.tests:  # 遍历所有测试
         Features = None
         for tag in test.tags:
             if tag.startswith("feature:"):
@@ -29,12 +15,27 @@ if __name__ == '__main__':
                     Features = tag[len("feature:"):]
                 else:
                     Features = Features + "," + tag[len("feature:"):]
-
+        if "Build_ID" in os.environ:
+            m_Build_ID = os.environ["Build_ID"]
+        else:
+            m_Build_ID = None
+        if "Build_URL" in os.environ:
+            m_Build_URL = os.environ["Build_URL"]
+        else:
+            m_Build_URL = None
+        if "Label_ID" in os.environ:
+            m_Label_ID = os.environ["Label_ID"]
+        else:
+            m_Label_ID = None
+        if "Creator" in os.environ:
+            m_Creator = os.environ["Creator"]
+        else:
+            m_Creator = None
         data = {
-            "Build_ID": os.environ["Build_ID"],
-            "Build_URL": os.environ["Build_URL"],
-            "Label_ID": os.environ["Label_ID"],
-            "Creator":  os.environ["Creator"],
+            "Build_ID": m_Build_ID,
+            "Build_URL": m_Build_URL,
+            "Label_ID": m_Label_ID,
+            "Creator": m_Creator,
             "Test_Name": test.name,
             "Test_Status": test.status,
             "Feature_ID": str(Features),
@@ -45,10 +46,34 @@ if __name__ == '__main__':
         }
         columns = ",".join(data.keys())
         values = ', '.join(['%s'] * len(data))
-        sql = "INSERT INTO TestJobs(JOB_ID, {keys}) VALUES (nextval('seq_testjobs'), {values})"\
+        sql = "INSERT INTO TestJobs(JOB_ID, {keys}) VALUES (nextval('seq_testjobs'), {values})" \
             .format(keys=columns, values=values)
-        mycursor = mydb.cursor()
+        mycursor = p_dbHandler.cursor()
         mycursor.execute(sql, tuple(data.values()))
-        print('Successful')
-        mydb.commit()
+        p_dbHandler.commit()
         mycursor.close()
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("Missed parameter:  ParseRobotOutput [output.xml]")
+        sys.exit(0)
+
+    # 解析XML文件
+    suite = ExecutionResult(sys.argv[1]).suite
+    #Results = ExecutionResult("C:\\Work\\linkoop\\linkoop-auto-test\\linkoopdb\\regression\\work\\output.xml").suite
+    # 连接mysql数据库
+    mydb = mysql.connector.connect(
+        host="node10",
+        user="report",
+        passwd="123456",
+        database="report"
+    )
+
+    if len(Results.suites) == 0:
+        load_testresults(mydb, Results)
+    else:
+        for suite in Results.suites:
+            load_testresults(mydb, suite)
+    mydb.close()
+    print('Successful')
