79f189e7c3f78329c9f5cdee56af16f2f884f583
robotslacker <184902652@qq.com>
2020-06-28 11:23:59 +0800

    update tpch

--- linkoopdb/regression/common/ReportJob.sh
+++ linkoopdb/regression/common/ReportJob.sh
@@ -12,7 +12,11 @@ set +x
 rm -f "$T_WORK"/output.xml
 rm -f "$T_WORK"/log.html
 rm -f "$T_WORK"/report.html
+echo "Will merge all rebot report files to one ... "
+echo $REBOT_BIN -d "$T_WORK" -o "$T_WORK"/output.xml "$T_WORK"/output[0-9]*.xml
 $REBOT_BIN -d "$T_WORK" -o "$T_WORK"/output.xml "$T_WORK"/output[0-9]*.xml || true
 
 # 将测试结果数据插入到统计数据库中
+echo "Will insert into robot test result to report database ..."
+echo $PY_BIN "$TEST_ROOT"/regression/common/ParseRobotOutput.py "$T_WORK"/output.xml
 $PY_BIN "$TEST_ROOT"/regression/common/ParseRobotOutput.py "$T_WORK"/output.xml
