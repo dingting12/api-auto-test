5a93d12817a13f36ae1cd00947f3a8b2ea9e6417
robotslacker <184902652@qq.com>
2020-06-30 10:44:40 +0800

    Fix Demo

--- linkoopdb/regression/common/RunJob2.sh
+++ linkoopdb/regression/common/RunJob2.sh
@@ -23,16 +23,22 @@ TEST_NAME=${TEST_NAME%.*}
 export T_WORK=$T_UP_T_WORK/sub_$TEST_NAME
 mkdir -p $T_WORK
 
-echo ======================================
-echo $TEST_NAME
-echo $T_WORK
-echo $MAX_PROCESSES
-echo ======================================
-
-# 开始i运行脚本
-cd "$(dirname "$1")" || { echo "Failed to enter test directory"; exit 1; }
-echo "$ROBOT_BIN" --listener allure_robotframework\;"$T_WORK" --loglevel DEBUG:INFO\
-    --outputdir "$T_WORK" -o "$m_output_xmlfilename" "$1"
-$ROBOT_BIN --listener allure_robotframework\;"$T_WORK" --loglevel DEBUG:INFO\
-    --outputdir "$T_WORK" -o "$m_output_xmlfilename" "$1"
-
+# 进入程序目录，开始运行程序
+cd "$(dirname "$TEST_MAIN")" || { echo "Failed to enter test directory"; exit 1; }
+echo "Run test case $TEST_MAIN ... "
+if [ "$MAX_PROCESSES" -eq 1 ]; then
+    echo "$ROBOT_BIN" --listener allure_robotframework\;"$T_WORK" --loglevel DEBUG:INFO\
+        --outputdir "$T_WORK" -o "$m_output_xmlfilename" "$TEST_MAIN"
+    $ROBOT_BIN --listener allure_robotframework\;"$T_WORK" --loglevel DEBUG:INFO\
+        --outputdir "$T_WORK" -o "$m_output_xmlfilename" "$TEST_MAIN"
+else
+    # 不再打印屏幕输出，而是直接输出信息到__stdout,以及__stderr中
+    echo "Run test case in queue $TEST_MAIN ... "
+    $ROBOT_BIN --listener allure_robotframework\;"$T_WORK" --loglevel DEBUG:INFO --outputdir "$T_WORK" \
+          "$TEST_MAIN" > "$T_WORK"/"$TEST_NAME".__stdout 2 > "$T_WORK"/"$TEST_NAME".__stderr || true &
+    push $!
+    while [[ "$run" -ge $MAX_PROCESSES ]];do
+        check
+        sleep 1
+    done
+fi
