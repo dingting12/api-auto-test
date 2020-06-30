45266e99aa9307e6a9ee7b27c7456a02506f2c61
dingting12 <dingting25@163.com>
2020-06-30 16:22:09 +0800

    提交外部表的测试用例

--- linkoopdb/regression/sql/external/decimal/external_table_basic_type_hdfs_parquet_decimal.sql
+++ linkoopdb/regression/sql/external/decimal/external_table_basic_type_hdfs_parquet_decimal.sql
@@ -23,80 +23,80 @@ drop table if exists t_external_hdfs_parquet_decimal_011;
 drop table if exists t_external_hdfs_parquet_decimal_012;
 
 
--- 创建表t_external_ldbdist_csv_numeric_001，测试总长度n>38,创建失败,实际成功
+-- 创建表t_external_ldbdist_csv_decimal_001，测试总长度n>38,创建失败,实际成功
 create external table t_external_hdfs_parquet_decimal_001(
 a1 decimal(39,1)
-)location('HDFSRPC_URL/numeric1_parquet')
+)location('HDFSRPC_URL/decimal1_parquet')
 FORMAT'parquet';
 
 -- 测试总长度n<1,创建失败，实际创建失败，报precision or scale out of range in statement
 create external table t_external_hdfs_parquet_decimal_002(
 a1 decimal(0,0) 
-)location('HDFSRPC_URL/numeric1_parquet')
+)location('HDFSRPC_URL/decimal1_parquet')
 FORMAT'parquet';
 
 -- 测试整数部分长度>n-m,例如234.01，应报错,实际返回空值
 create external table t_external_hdfs_parquet_decimal_003(
 a1 decimal(4,2)  
-)location('HDFSRPC_URL/numeric1_parquet')
+)location('HDFSRPC_URL/decimal1_parquet')
 FORMAT'parquet';
   
---查询t_external_ldbdist_csv_numeric_003的数据
+--查询t_external_ldbdist_csv_decimal_003的数据
 select * from t_external_hdfs_parquet_decimal_003;
 
 
 --测试m小于0,创建失败，实际报错precision or scale out of range in statement
 create external table t_external_hdfs_parquet_decimal_004(
 a1 decimal(2,-1)  
-)location('HDFSRPC_URL/numeric1_parquet')
+)location('HDFSRPC_URL/decimal1_parquet')
 FORMAT'parquet';
 
 
 --测试m大于n,创建失败，实际报错precision or scale out of range in statement
 create external table t_external_hdfs_parquet_decimal_005(
 a1 decimal(3,6)  
-)location('HDFSRPC_URL/numeric1_parquet')
+)location('HDFSRPC_URL/decimal1_parquet')
 FORMAT'parquet';
 
 
 -- 测试(*,m),创建失败，实际报错 unexpected token: * 
 create external table t_external_hdfs_parquet_decimal_006(
 a1 decimal(*,m)  
-)location('HDFSRPC_URL/numeric1_parquet')
+)location('HDFSRPC_URL/decimal1_parquet')
 FORMAT'parquet';
 
 --测试(*),创建失败，实际报错unexpected token: * 
 create external table t_external_hdfs_parquet_decimal_007(
 a1 decimal(*)  
-)location('HDFSRPC_URL/numeric1_parquet')
+)location('HDFSRPC_URL/decimal1_parquet')
 FORMAT'parquet';
   
--- 测试numeric(n),n设置为39,创建报错，实际创建成功
+-- 测试decimal(n),n设置为39,创建报错，实际创建成功
 create external table t_external_hdfs_parquet_decimal_008(
 a1 decimal(39)  
-)location('HDFSRPC_URL/numeric1_parquet')
+)location('HDFSRPC_URL/decimal1_parquet')
 FORMAT'parquet';
   
   
 
 
---测试numeric默认值是否为(5,0),值为123456，实际为123456
+--测试decimal默认值是否为(5,0),值为123456，实际为123456
 create external table t_external_hdfs_parquet_decimal_009(
 a1 decimal  
-)location('HDFSRPC_URL/numeric2_parquet')
+)location('HDFSRPC_URL/decimal2_parquet')
 FORMAT'parquet';
 
 select * from t_external_hdfs_parquet_decimal_009;
 
---测试numeric(n),n设置为5,值为123456大于5，报错，实际返回空值 
+--测试decimal(n),n设置为5,值为123456大于5，报错，实际返回空值 
 create external table t_external_hdfs_parquet_decimal_010(
 a1 decimal(5)  
-)location('HDFSRPC_URL/numeric2_parquet')
+)location('HDFSRPC_URL/decimal2_parquet')
 FORMAT'parquet';
   
 select * from t_external_hdfs_parquet_decimal_010;
 
---测试测试小数部分长度>m，测试numeric默认值是否为(5,0),测试int，varchar
+--测试测试小数部分长度>m，测试decimal默认值是否为(5,0),测试int，varchar
 create external table t_external_hdfs_parquet_decimal_011(
 a1 decimal(4,2),
 a2 decimal,
@@ -105,7 +105,7 @@ a4 decimal(5),
 a5 decimal(5),
 a6 decimal(20) ,
 a7 decimal(20),
-)location('HDFSRPC_URL/numeric3_parquet')
+)location('HDFSRPC_URL/decimal3_parquet')
 FORMAT'parquet';
   
 select * from t_external_hdfs_parquet_decimal_011;
@@ -127,6 +127,6 @@ a11 decimal,
 a12 decimal,
 a13 decimal
 )
-LOCATION ('HDFSRPC_URL/common_parquet') FORMAT'parquet';
+LOCATION ('HDFSRPC_URL/common_decimal_parquet') FORMAT'parquet';
 
 select * from t_external_hdfs_parquet_decimal_012;
