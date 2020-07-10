818d8cd435d6791e2bfe8db5c7d4aadb5c48bc54
robotslacker <184902652@qq.com>
2020-07-10 17:35:59 +0800

    add for ldb dist support

--- linkoopdb/regression/common/ParseRobotOutput.py
+++ linkoopdb/regression/common/ParseRobotOutput.py
@@ -6,8 +6,8 @@ import os
 import sys
 
 
-def load_testresults(p_dbHandler, p_szSuite):
-    for test in p_szSuite.tests:  # 遍历所有测试
+def load_testresults(p_dbHandler, p_SuiteResults, p_PerfResults):
+    for test in p_SuiteResults.tests:  # 遍历所有测试
         Features = None
         for tag in test.tags:
             if tag.startswith("feature:"):
@@ -31,6 +31,16 @@ def load_testresults(p_dbHandler, p_szSuite):
             m_Creator = os.environ["Creator"]
         else:
             m_Creator = None
+        m_SuiteAndTestName = os.path.basename(test.source) + ":" + test.name
+
+        if m_SuiteAndTestName in p_PerfResults.keys():
+            m_SQLCount = p_PerfResults[m_SuiteAndTestName]
+        else:
+            m_SQLCount = 0
+        if "DATABASE_VERSION" in os.environ:
+            m_DATABASE_VERSION = os.environ["DATABASE_VERSION"]
+        else:
+            m_DATABASE_VERSION = "Unknown"
         data = {
             "Build_ID": m_Build_ID,
             "Build_URL": m_Build_URL,
@@ -43,8 +53,8 @@ def load_testresults(p_dbHandler, p_szSuite):
             "Test_Doc": str(test.doc),
             "Start_Date": datetime.datetime.strptime(test.starttime[0:17], '%Y%m%d %H:%M:%S'),
             "End_Date": datetime.datetime.strptime(test.endtime[0:17], '%Y%m%d %H:%M:%S'),
-            "Database_Version": os.environ["DATABASE_VERSION"],
-            "SQL_Count": int(os.environ["SQL_COUNT"])
+            "Database_Version": m_DATABASE_VERSION,
+            "SQL_Count": m_SQLCount,
         }
         columns = ",".join(data.keys())
         values = ', '.join(['%s'] * len(data))
@@ -57,13 +67,29 @@ def load_testresults(p_dbHandler, p_szSuite):
 
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print("Missed parameter:  ParseRobotOutput [output.xml]")
+    if len(sys.argv) < 3:
+        print("Missed parameter:  ParseRobotOutput [output.xml] [output.perf]")
         sys.exit(0)
 
+    # RobotResults = ExecutionResult("C:\\Work\\linkoop\\linkoop-auto-test\\linkoopdb\\regression\\work\\output.xml").suite
+    # with open("C:\\Work\\linkoop\\linkoop-auto-test\\linkoopdb\\regression\\work\\output.perf", 'r') as f:
+
     # 解析XML文件
-    Results = ExecutionResult(sys.argv[1]).suite
-    # Results = ExecutionResult("C:\\Work\\linkoop\\linkoop-auto-test\\linkoopdb\\regression\\work\\output.xml").suite
+    RobotResults = ExecutionResult(sys.argv[1]).suite
+    with open(sys.argv[2], 'r') as f:
+        PerfLines = f.readlines()
+    PerfResults = {}
+    for row in PerfLines:
+        m_rowCotents = row.split('\t')
+        if len(m_rowCotents) > 6:
+            m_SuiteAndTest = row.split('\t')[6]
+            if len(m_SuiteAndTest.split(":")) == 2:
+                m_SuiteAndTest = m_SuiteAndTest.replace("'", "")      # 去掉字符串的单引号
+                if m_SuiteAndTest in PerfResults.keys():
+                    PerfResults[m_SuiteAndTest] = PerfResults[m_SuiteAndTest] + 1
+                else:
+                    PerfResults[m_SuiteAndTest] = 1
+
     # 连接mysql数据库
     mydb = mysql.connector.connect(
         host="node10",
@@ -72,10 +98,10 @@ if __name__ == '__main__':
         database="report"
     )
 
-    if len(Results.suites) == 0:
-        load_testresults(mydb, Results)
+    if len(RobotResults.suites) == 0:
+        load_testresults(mydb, RobotResults, PerfResults)
     else:
-        for suite in Results.suites:
-            load_testresults(mydb, suite)
+        for suite in RobotResults.suites:
+            load_testresults(mydb, suite, PerfResults)
     mydb.close()
     print('Successful')
