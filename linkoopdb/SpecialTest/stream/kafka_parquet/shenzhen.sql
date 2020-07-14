8f0e3a556a208d37547b54d18ef39c1093d264a3
luyao1123 <luyao.jia@datapps.com>
2020-07-14 18:26:14 +0800

    提交专项测试用例

---
+++ linkoopdb/SpecialTest/stream/kafka_parquet/shenzhen.sql
@@ -0,0 +1,150 @@
+__internal__ create fs file temp.csv
+(
+{random_ascii_letters_and_digits(17)},
+{random_digits(10)},
+{random_digits(10)},
+{random_digits(10)},
+{random_digits(6)},
+{random_digits(6)},
+{random_digits(6)},
+{random_digits(10)},
+{random_ascii_letters_and_digits(50)},
+{random_ascii_letters_and_digits(100)},
+{random_ascii_letters_and_digits(100)},
+{random_ascii_letters_and_digits(100)},
+{random_ascii_letters_and_digits(16)},
+{random_ascii_letters_and_digits(16)},
+{random_digits(10)},
+{random_digits(10)},
+{random_digits(10)},
+{random_digits(10)},
+{random_digits(10)},
+{random_digits(10)},
+{random_ascii_letters_and_digits(50)},
+{random_ascii_letters_and_digits(50)},
+{random_ascii_letters_and_digits(50)},
+{random_ascii_letters_and_digits(50)},
+{current_timestamp()}
+) rows 10000;
+__internal__ create kafka server node10:9092;
+__internal__ drop kafka topic shenzhen;
+__internal__ create kafka topic shenzhen Partitions 20 replication_factor 1;
+sleep 3
+__internal__ create kafka message from file temp.csv to topic shenzhen;
+
+
+CREATE stream s_kafka_source(
+TS_15M VARCHAR(17),
+MSISDN BIGINT,
+IMEI BIGINT,
+IMSI BIGINT,
+MCC INTEGER,
+MNC INTEGER,
+LAC INTEGER,
+CELL BIGINT,
+HOST VARCHAR(128),
+URI VARCHAR(10240),
+L7REQ VARCHAR(1024),
+L7REP VARCHAR(1024),
+LATITUDE VARCHAR(16),
+LONGITUDE VARCHAR(16),
+CILENTBYTE BIGINT,
+SERVERBYTE BIGINT,
+CLIENTFIRSTSEC BIGINT,
+SERVERFIRSTSEC BIGINT,
+CLIENTLASTSEC BIGINT,
+SERVERLASTSEC BIGINT,
+CLIENTIPTO VARCHAR(128),
+SERVERIPTO VARCHAR(128),
+CLIENTIP VARCHAR(128),
+SERVERIP VARCHAR(128),
+ctime timestamp
+)properties(
+ 'type': 'source',
+ 'connector': 'kafka',
+ 'version': 'universal',
+ 'topic': 'shenzhen',
+ 'group.id': 'random_str',
+ 'proctime':'atime',
+ 'bootstrap.servers': 'node10:9092',
+ 'format':'csv',
+ 'separator':'|'
+);
+
+CREATE STREAM S_PARQUET_SINK(
+TS_15M VARCHAR(17),
+MSISDN BIGINT,
+IMEI BIGINT,
+IMSI BIGINT,
+MCC INTEGER,
+MNC INTEGER,
+LAC INTEGER,
+CELL BIGINT,
+HOST VARCHAR(128),
+URI VARCHAR(10240),
+L7REQ VARCHAR(1024),
+L7REP VARCHAR(1024),
+LATITUDE VARCHAR(16),
+LONGITUDE VARCHAR(16),
+CILENTBYTE BIGINT,
+SERVERBYTE BIGINT,
+CLIENTFIRSTSEC BIGINT,
+SERVERFIRSTSEC BIGINT,
+CLIENTLASTSEC BIGINT,
+SERVERLASTSEC BIGINT,
+CLIENTIPTO VARCHAR(128),
+SERVERIPTO VARCHAR(128),
+CLIENTIP VARCHAR(128),
+SERVERIP VARCHAR(128),
+ctime timestamp,
+atime timestamp
+) PROPERTIES(
+   'type': 'sink',
+   'connector':'filesystem',
+   'format':'parquet',
+   'path':'hdfs:///node62/shenzhen',
+   'write-mode':'time',
+   'batch-rollover-interval':'1D',
+   'time-format':'yyyy-mm-dd',
+   'separator': '-',
+   'time-path-prefix': 'test_',
+   'time-path-suffix': '_table',
+   'inactive-bucket-threshold':'10',
+   'inactive-bucket-check-interval':'30'
+);
+
+set session STREAM_EXECUTE_PARALLELISM 20;
+
+insert into S_PARQUET_SINK select * from s_kafka_source;
+
+CREATE EXTERNAL TABLE t_parquet(
+TS_15M VARCHAR(17),
+MSISDN BIGINT,
+IMEI BIGINT,
+IMSI BIGINT,
+MCC INTEGER,
+MNC INTEGER,
+LAC INTEGER,
+CELL BIGINT,
+HOST VARCHAR(128),
+URI VARCHAR(10240),
+L7REQ VARCHAR(1024),
+L7REP VARCHAR(1024),
+LATITUDE VARCHAR(16),
+LONGITUDE VARCHAR(16),
+CILENTBYTE BIGINT,
+SERVERBYTE BIGINT,
+CLIENTFIRSTSEC BIGINT,
+SERVERFIRSTSEC BIGINT,
+CLIENTLASTSEC BIGINT,
+SERVERLASTSEC BIGINT,
+CLIENTIPTO VARCHAR(128),
+SERVERIPTO VARCHAR(128),
+CLIENTIP VARCHAR(128),
+SERVERIP VARCHAR(128),
+ctime timestamp,
+atime timestamp
+)LOCATION ('hdfs:///node62/shenzhen') FORMAT 'parquet';
+
+select * from t_parquet;
+
